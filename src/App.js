f1d2835474746b81b2e2e1112f571ac092bea549
Tejus <apple@Admins-MacBook-Pro.local>
2020-03-31 18:51:04 +0530

    done 81 - redux store and provider setup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import SearchBar from './components/layout/SearchBar'
 import Logs from './components/logs/Logs'
 import AddBtn from './components/layout/AddBtn'
 import AddLogModal from './components/logs/AddLogModal.js'
-import AddTechModal from './components/techs/AddtechModal'
-import TechListModal from './components/techs/TechListModal'
+import AddTechModal from './components/techs/AddtechModal.js'
+import TechListModal from './components/techs/TechListModal.js'
 import EditLogModal from './components/logs/EditLogModal.js'
+// Import provider and store for redux
+import { Provider } from 'react-redux';
+import store from './store';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -18,18 +21,19 @@ const App = () => {
   })
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <Logs />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-      </div>
-
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <Logs />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
